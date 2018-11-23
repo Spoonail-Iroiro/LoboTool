b64ec0ba86e2183e1c430e8624524f8dc0d6a4b6
saji-spoon <tkgkmny0707@hotmail.co.jp>
2018-11-24 01:01:25 +0900

    Add readme_md

---
+++ SQL/restore_savs.sql
@@ -0,0 +1,14 @@
+UPDATE m_work_aptitude
+SET 
+inst_code = "00",
+insi_code = "00",
+atta_code = "00",
+supr_code = "00",
+remarks = ""
+
+
+DELETE 
+FROM sav_employee
+
+DELETE
+FROM sav_abnormality
--- env.bat
+++
@@ -1,6 +0,0 @@
-set SCRIPT_PATH = C:\Users\spoon\Anaconda3\envs\pyLobo\Library\bin\
-set PATH=%SCRIPT_PATH%;%PATH%
-
-C:\Users\spoon\Anaconda3\Scripts\activate.bat C:\Users\spoon\Anaconda3\envs\pyLobo
-
-chcp 65001
